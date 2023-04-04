f00b92daeb5424f0c6140cacd885a37b36bc4811
Muhamad Arwin Wijaya <54936971+marwinwijaya@users.noreply.github.com>
2023-04-04 11:13:02 +0700

    Prettier

--- js/script.js
+++ js/script.js
@@ -1,45 +1,43 @@
 $(document).ready(function () {
-    // grab the initial top offset of the navigation 
-    var stickyNavTop = $('.nav').offset().top;
+  // grab the initial top offset of the navigation
+  var stickyNavTop = $(".nav").offset().top;
 
-    // our function that decides weather the navigation bar should have "fixed" css position or not.
-    var stickyNav = function () {
-        var scrollTop = $(window).scrollTop(); // our current vertical position from the top
+  // our function that decides weather the navigation bar should have "fixed" css position or not.
+  var stickyNav = function () {
+    var scrollTop = $(window).scrollTop(); // our current vertical position from the top
 
-        // if we've scrolled more than the navigation, change its position to fixed to stick to top,
-        // otherwise change it back to relative
-        if (scrollTop > stickyNavTop) {
-            $('.nav').addClass('sticky');
-        } else {
-            $('.nav').removeClass('sticky');
-        }
-    };
+    // if we've scrolled more than the navigation, change its position to fixed to stick to top,
+    // otherwise change it back to relative
+    if (scrollTop > stickyNavTop) {
+      $(".nav").addClass("sticky");
+    } else {
+      $(".nav").removeClass("sticky");
+    }
+  };
 
+  stickyNav();
+  // and run it again every time you scroll
+  $(window).scroll(function () {
     stickyNav();
-    // and run it again every time you scroll
-    $(window).scroll(function () {
-        stickyNav();
-    });
+  });
 });
-
 
 $(".scroll").on("click", function (event) {
-    event.preventDefault();
-    $('html,body').animate({
-        scrollTop: $(this.hash).offset().top - 130
-    });
+  event.preventDefault();
+  $("html,body").animate({
+    scrollTop: $(this.hash).offset().top - 130,
+  });
 });
 
-$('#navbarSupportedContent .nav-item .nav-link').on('click', function () {
-    $('.navbar-collapse').collapse('hide');
+$("#navbarSupportedContent .nav-item .nav-link").on("click", function () {
+  $(".navbar-collapse").collapse("hide");
 });
 
 // atur waktu akhir countdown
 var countDownDate = new Date("June 28, 2023 00:00:00").getTime();
 
 // perbarui countdown setiap 1 detik
-var x = setInterval(function() {
-
+var x = setInterval(function () {
   // dapatkan waktu saat ini
   var now = new Date().getTime();
 
@@ -53,12 +51,13 @@ var x = setInterval(function() {
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
   // tampilkan waktu pada elemen countdown timer
-  document.getElementById("countdown-timer").innerHTML = days + "d " + hours + "h "
-  + minutes + "m " + seconds + "s ";
+  document.getElementById("countdown-timer").innerHTML =
+    days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
 
   // jika waktu countdown telah habis, tampilkan pesan
   if (distance < 0) {
     clearInterval(x);
-    document.getElementById("countdown-timer").innerHTML = "Selamat Hari Raya Idul Adha 2023.";
+    document.getElementById("countdown-timer").innerHTML =
+      "Selamat Hari Raya Idul Adha 2023.";
   }
 }, 1000);
